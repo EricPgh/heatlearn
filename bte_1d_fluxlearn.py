8e6ff72c4f01d29b6fb514898015add60fee9be0
EricPgh <49766504+EricPgh@users.noreply.github.com>
2025-10-21 10:14:14 -0400

    testing major_stride
    Testing sensitivity of integration accuracy to major_stride and point in time

--- bte_1d_fluxlearn.py
+++ bte_1d_fluxlearn.py
@@ -238,45 +238,82 @@ if bPlot:
     #block6
     #my best effort so far is this block which demonstrates propagation by forward euler
     fig, axs = plt.subplots(figsize=(6,4))
-nA = 13000//major_stride #The quantity of propagators to compute
-lAc = [] #The running list of each propagator, quantity nA
-lTimes = []
-#I'm wondering if this compression scheme is appropriate the block matrix operator that is occuring with [N;F] concat
-print(len(GN),len(GF),len(range(strt+i*major_stride  ,strt+i*major_stride+npts*minor_stride  ,minor_stride)))
-for i in range(nA):
-    #Gt.append(G[i:11+i])
-    #Again using a second order scheme for integrating
-    N0 = np.array(GN[strt+i*major_stride  :strt+i*major_stride+npts*minor_stride  :minor_stride]).T
-    F0 = np.array(GF[strt+i*major_stride  :strt+i*major_stride+npts*minor_stride  :minor_stride]).T
-    #print(N0.shape,F0.shape)
-    X  = np.concatenate((N0,F0),axis=0) #The priors X, are a composite of two coupled observations [N0;F0], both contributing to the prediction of Y, hence the A matrix has twice the size containing how Y evolves from each group of prior observations
-    #print(i,X.shape)
-    #Here Y is defined as the rate of change of observations. Alternatively could use just an observation of the system, but the physics being studied are nonlinear second order ODE (or nonlinear system of two first order ODE)
-    N1 = np.array(GN[strt+i*major_stride+minor_stride:strt+i*major_stride+npts*minor_stride+minor_stride:minor_stride]).T
-    F1 = np.array(GF[strt+i*major_stride+minor_stride:strt+i*major_stride+npts*minor_stride+minor_stride:minor_stride]).T
-    dN = (N1-N0)/minor_stride
-    dF = (F1-F0)/minor_stride
-    Y  = np.concatenate((dN,dF),axis=0) #The posteriors Y, are a composite of two coupled difference observations [dN/dt;dF/dt], e.g. the state of Y, hence the A matrix has 4x4 block form containing how Y evolves from each group of prior observations
-    #It can be seen that the Y observation is X1-X0 while the X observation is X0. Thus the system behavior being trained is X1-X0 = A(X0). Given X0, the A propagator will yield X1-X0 and then X0. This Y observation is divided by the minor stride timestep to yield a proper first derivative approximate
-    u,s,vt = svd(X,full_matrices=False) #full matrices=false, want to be able to compress, S has nonzero only
-    #s += 0.001
-    #print(s)
-    #A = Y@v.T@np.diag(sinv)@u.T
-    X_pinv = pseudo_inverse_from_svd(u, s, vt, cmp) # v.T[:,0:cmp]@np.diag(sinv[0:cmp])@u.T[0:cmp,:]
-    Ac = Y@X_pinv #taking the 50% compression, this also seems to stabilise the approximation, maybe consider a study on the variation of this with integration accuracy
-    lAc.append(Ac) #this will contain a time evolution of differential propagators
-    #lTimes.append(ts[strt+i*major_stride+minor_stride]/2+ts[strt+i*major_stride+npts*minor_stride]/2) #here I'm sampling times at the same rate (major_stride) such that I have time positions aligned with Ac transforms
-    lTimes.append(ts[strt+i*major_stride])#/2+ts[strt+(i+1)*major_stride]/2) #here I'm sampling times at the same rate (major_stride) such that I have time positions aligned with Ac transforms
-    #This might not be occuring the way I imagine it. Maybe I should just get the delta_t and increment that in the integration loop
-#print(lTimes[0])
 
-import numpy as np
-from scipy.integrate import solve_ivp
 #from linear_interp import Propagator
 from cubic_interp import Propagator
-prop = Propagator()
-prop.major_stride = major_stride
-prop.buildMe(lTimes,lAc)
+props = [Propagator() for _ in range(2)]
+
+for j,major_stride in enumerate([4,16]):
+    props[j].major_stride = major_stride
+    nA = 13000//major_stride #The quantity of propagators to compute
+    lAc = [] #The running list of each propagator, quantity nA
+    lTimes = []
+    strt = 1
+    minor_stride = 1 #not skipping timesteps within prior/posterior observations
+    npts = 2 #keeping the number of observations small per propagator, trying to make the propagator see the system as nearly a linear change
+    #I'm wondering if this compression scheme is appropriate the block matrix operator that is occuring with [N;F] concat
+    cmp=1 #fidelity or compression again, up to npts
+    #I'm wondering if this compression scheme is appropriate the block matrix operator that is occuring with [N;F] concat
+    #print(len(GN),len(GF),len(range(strt+i*major_stride  ,strt+i*major_stride+npts*minor_stride  ,minor_stride)))
+    for i in range(nA):
+        #Gt.append(G[i:11+i])
+        #Again using a second order scheme for integrating
+        N0 = np.array(GN[strt+i*major_stride  :strt+i*major_stride+npts*minor_stride  :minor_stride]).T
+        F0 = np.array(GF[strt+i*major_stride  :strt+i*major_stride+npts*minor_stride  :minor_stride]).T
+        #print(N0.shape,F0.shape)
+        X  = np.concatenate((N0,F0),axis=0) #The priors X, are a composite of two coupled observations [N0;F0], both contributing to the prediction of Y, hence the A matrix has twice the size containing how Y evolves from each group of prior observations
+        #print(i,X.shape)
+        #Here Y is defined as the rate of change of observations. Alternatively could use just an observation of the system, but the physics being studied are nonlinear second order ODE (or nonlinear system of two first order ODE)
+        N1 = np.array(GN[strt+i*major_stride+minor_stride:strt+i*major_stride+npts*minor_stride+minor_stride:minor_stride]).T
+        F1 = np.array(GF[strt+i*major_stride+minor_stride:strt+i*major_stride+npts*minor_stride+minor_stride:minor_stride]).T
+        dN = (N1-N0)/minor_stride
+        dF = (F1-F0)/minor_stride
+        Y  = np.concatenate((dN,dF),axis=0) #The posteriors Y, are a composite of two coupled difference observations [dN/dt;dF/dt], e.g. the state of Y, hence the A matrix has 4x4 block form containing how Y evolves from each group of prior observations
+        #It can be seen that the Y observation is X1-X0 while the X observation is X0. Thus the system behavior being trained is X1-X0 = A(X0). Given X0, the A propagator will yield X1-X0 and then X0. This Y observation is divided by the minor stride timestep to yield a proper first derivative approximate
+        u,s,vt = svd(X,full_matrices=False) #full matrices=false, want to be able to compress, S has nonzero only
+        #s += 0.001
+        #print(s)
+        #A = Y@v.T@np.diag(sinv)@u.T
+        X_pinv = pseudo_inverse_from_svd(u, s, vt, cmp) # v.T[:,0:cmp]@np.diag(sinv[0:cmp])@u.T[0:cmp,:]
+        Ac = Y@X_pinv #taking the 50% compression, this also seems to stabilise the approximation, maybe consider a study on the variation of this with integration accuracy
+        lAc.append(Ac) #this will contain a time evolution of differential propagators
+        #lTimes.append(ts[strt+i*major_stride+minor_stride]/2+ts[strt+i*major_stride+npts*minor_stride]/2) #here I'm sampling times at the same rate (major_stride) such that I have time positions aligned with Ac transforms
+        lTimes.append(ts[strt+i*major_stride])#/2+ts[strt+(i+1)*major_stride]/2) #here I'm sampling times at the same rate (major_stride) such that I have time positions aligned with Ac transforms
+        #This might not be occuring the way I imagine it. Maybe I should just get the delta_t and increment that in the integration loop
+    #print(lTimes[0])
+    prop.buildMe(lTimes,lAc)
+
+def plot_A_elements(props,ts, n_points=200):
+    """Plot all A_ij(t) trajectories from the Propagator instance."""
+    t_dense = np.linspace(ts[0], ts[1], n_points)
+    n0, n1 = props[0].dim0, props[0].dim1
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    colormap = plt.get_cmap('tab10', n0*n1)
+    colors = [colormap(k) for k in range(n0*n1)] #when plotting exact vs approx curves, each epoch has own color from cmap
+    for i in range(n0):
+        for j in range(n1):
+            col = colors[i*n1+j]
+            A_vals = [ [props[k].splines[i][j](t) for t in t_dense] for k in range(2)]
+            #ax.plot(t_dense, A_vals, label=f"A[{i},{j}]")
+            ax.plot(A_vals[0], A_vals[1], linestyle='',marker='+',color=col)
+
+    ax.set_xlabel("Time")
+    ax.set_ylabel("A(t) elements")
+    ax.set_title("Time evolution of A(t) matrix elements")
+    ax.legend(fontsize=8, ncol=3, loc="upper right", bbox_to_anchor=(1.3, 1))
+    plt.tight_layout()
+    plt.xlim(-.004,.005)
+    plt.ylim(-.004,.005)
+    plt.show()
+
+plot_A_elements(props,[0.,0.2])
+plot_A_elements(props,[0.2,min([props[j].t_list[-1] for j in range(2)])])
+
+prop = props[1]
+import numpy as np
+from scipy.integrate import solve_ivp
+
 
 
 #With propagators formed, here begins the forward euler loop. this is intended to operate between large strides in the pickle solutions, starting with the beginning conditions of one stride and matching the final solutions (from BTE) at the end of the stride
